33061b76b91e06d28f6fac2600bce8b73decb480
Admin <admin@192.168.25.131>
2017-11-28 20:35:33 +0200

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Admin on 11/28/17.
+//  Copyright © 2017 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
